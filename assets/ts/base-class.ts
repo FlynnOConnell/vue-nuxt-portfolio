22dd62eb912c87ffb3554ebf0d5d85cde2e30871
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-14 01:54:23 -0400

    add logging functionality

---
+++ assets/ts/base-class.ts
@@ -0,0 +1,43 @@
+import { ILogger } from 'typescript-ilogger';
+import { LogHelper } from './log-helper';
+import { ObjectOperations } from './object-operations';
+import { Orchestrator } from './orchestrator';
+
+/**
+ * Base class
+ */
+export class BaseClass {
+  /**
+   * Logger
+   */
+  public Logger: ILogger;
+
+  /**
+   * Log Helper
+   */
+  public LogHelper: LogHelper;
+
+  /**
+   * Object Operations
+   */
+  public ObjectOperations: ObjectOperations;
+
+  /**
+   * Orchestrator
+   */
+  public Orchestrator: Orchestrator;
+
+  /**
+   * Initialize new instance of BaseClass
+   * @param logger {ILogger} Logger
+   */
+  constructor(logger: ILogger) {
+    this.Logger = logger;
+
+    this.LogHelper = new LogHelper(this.Logger);
+
+    this.ObjectOperations = new ObjectOperations();
+
+    this.Orchestrator = new Orchestrator(this.Logger);
+  }
+}
