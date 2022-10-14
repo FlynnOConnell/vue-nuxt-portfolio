e1da9485c7bad2f0bf9fceaf1dbf3b3787cc89ef
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-14 01:54:38 -0400

    stick footer to bottom

---
+++ components/bottomfooter.vue
@@ -0,0 +1,16 @@
+<template>
+	<footer>
+		<p class="text-center text-lg">Copyright &copy; 2022 Flynn OConnell</p>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "bottomfooter",
+};
+</script>
+
+<style scoped>
+
+
+</style>
