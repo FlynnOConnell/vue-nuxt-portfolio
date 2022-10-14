ee20d36f78dda90b8cf3e829cb77dc20af3454ae
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-14 12:45:48 -0400

    remove scroll-reveal dep. and add svg graphics

---
+++ static/drawsvg.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+// function myFunction(element) {
+
+   
+    
+//   }
+
+//   function mainFunction() {
+//     let active = $(this)
+//     let img
+//     if(active.attr("id") === "button-dev"){
+//         img = $("#resume")
+//         $("#resume2").addClass("hide-this")
+//         img.removeClass("hide-this")
+//     }
+//     else {
+//         img = $("#resume2")
+//         $("#resume").addClass("hide-this")
+//         img.removeClass("hide-this")
+//      }
+// }
+
+// function ready(callback){
+//     // in case the document is already rendered
+//     if (document.readyState!='loading') callback();
+//     // modern browsers
+//     else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+//     // IE <= 8
+//     else document.attachEvent('onreadystatechange', function(){
+//         if (document.readyState=='complete') callback();
+//     });
+// }
+
+// ready(function(){
+
+//     // svg element
+//     var mysvg = document.getElementById('signature')
+
+//     var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+//     svg.setAttribute('style', 'border: 1px solid black');
+//     svg.setAttribute('width', '600');
+//     svg.setAttribute('height', '250');
+//     svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+//     document.body.appendChild(svg);
+
+// });
